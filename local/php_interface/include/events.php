3b410a323f74ec714bfe4b95d488367ba7333952
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-10-03 19:16:56 +0300

    Ex2-93

--- local/php_interface/include/events.php
+++ local/php_interface/include/events.php
@@ -2,6 +2,7 @@
 IncludeModuleLangFile(__FILE__);
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", array("Ex2", "Ex2_50"));
 AddEventHandler("main", "OnBeforeEventAdd", array("Ex2", "Ex2_51"));
+AddEventHandler("main", "OnEpilog", array("Ex2", "Ex2_93"));
 
 class Ex2
 {
@@ -62,4 +63,23 @@ class Ex2
             );
         }
     }
+    function Ex2_93()
+	{
+		if (defined("ERROR_404") && ERROR_404 == "Y")
+		{
+			global $APPLICATION;
+			$APPLICATION->RestartBuffer();
+			include $_SERVER["DOCUMENT_ROOT"] . "/" . SITE_TEMPLATE_PATH . "/header.php";
+			include $_SERVER["DOCUMENT_ROOT"] . "/404.php";
+			include $_SERVER["DOCUMENT_ROOT"] . "/" . SITE_TEMPLATE_PATH . "/footer.php";
+			CEventLog::Add(
+				array(
+					"SEVERITY"	=>	"INFO",
+					"AUDIT_TYPE_ID"	=>	"ERROR_404",
+					"MODULE_ID"	=>	"main",
+					"DESCRIPTION"	=>	$APPLICATION->GetCurPage()
+				)
+            );
+		}
+	}
 }
