4d0499a6bf606f4e26b45bb087ab5c97b1c42092
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-10-03 17:08:55 +0300

    Ex2-108

--- local/templates/furniture_blue/components/bitrix/news/.default/lang/ru/.parameters.php
+++ local/templates/furniture_blue/components/bitrix/news/.default/lang/ru/.parameters.php
@@ -8,5 +8,6 @@ $MESS["T_IBLOCK_DESC_NEWS_SHARE_TEMPLATE"] = "Шаблон компонента 
 $MESS["T_IBLOCK_DESC_NEWS_SHARE_SYSTEM"] = "Используемые соц. закладки и сети";
 $MESS["T_IBLOCK_DESC_NEWS_SHARE_SHORTEN_URL_LOGIN"] = "Логин для bit.ly";
 $MESS["T_IBLOCK_DESC_NEWS_SHARE_SHORTEN_URL_KEY"] = "Ключ для для bit.ly";
+$MESS["ID_IBLOCK_CANONICAL"] = "ID информационного блока для rel=canonical";
 $MESS["SPECIALDATE"] = "Установить свойство страницы specialdate";
 ?>
